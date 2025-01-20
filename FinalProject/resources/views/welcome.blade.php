76259594a4f0817efa70645d0b674bef12a43309
SimonKhadka07 <ksimon22@tbc.edu.np>
2025-01-20 18:07:46 +0545

    rectified errors

--- FinalProject/resources/views/welcome.blade.php
+++ FinalProject/resources/views/welcome.blade.php
@@ -32,7 +32,7 @@
                             <nav class="-mx-3 flex flex-1 justify-end">
                                 @auth
                                     <a
-                                        href="{{ url('/dashboard') }}"
+                                        href="{{ url('/home') }}"
                                         class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
                                     >
                                         Dashboard
--- FinalProject/routes/web.php
+++ FinalProject/routes/web.php
@@ -1,20 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+// use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/home',[HomeController::class,'index']);
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
 
-});
+
+Route::get('/home',[AdminController::class,'index'])->name('home');
